82313b675c0bca6cff26f7bd7199d61f024341cd
alexgu0582 <guchunguang261982@gmail.com>
2024-03-27 12:22:49 +1100

    office Chapter12

--- Chapter12.sh
+++ Chapter12.sh
@@ -81,3 +81,36 @@ if [ condition ]
 then 
     commands
 fi 
+
+
+#The test Numeric Comparisons
+#n1 -eq n2  Checks if n1 is equal to n2
+#n1 -ge n2  Checks if n1 is greater than or equal to n2
+#n1 -gt n2  Checks if n1 is greater than n2
+#n1 -le n2  Checks if n1 is less than or equal to n2
+#n1 -lt n2  Checks if n1 is less than n2
+#n1 -ne n2  Checks if n1 is not equal to n2
+
+
+#The test String Comparisons
+#str1 = str2    Checks if str1 is the same as string str2
+#str1 != str2   Checks if str1 is not the same as str2
+#str1 < str2    Checks if str1 is less than str2
+#str1 >str2     Checks if str1 is greater than str2
+#-n str1        Checks if str1 has a length greater than zero
+#-z str1        Checks if str1 has a length of zero
+
+
+#The test File Comparisons
+#-d file        Checks if file exists and is a directory.
+#-e file        Checks if file exists.
+#-f file        Checks if file exists and is a file.
+#-r file        Checks if file exists and is readable.
+#-s file        Checks if file exists and is not empty.
+#-w file        Checks if file exists and is writable.
+#-x file        Checks if file exists and is executable.
+#-O file        Checks if file exists and is owned by the current user.
+#-G file        Checks if file exists and the default group is the same as the current user.
+#file1 -nt file2 Checks if file1 is newer than file2.
+#file1 -ot file Checks if file is older than file2.
+
